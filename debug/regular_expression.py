397cf5b6d1ae5281976ac284e5dc12b5b4e77ebc
Angus <1621xjx@buaa.edu.cn>
2018-02-10 23:56:08 +0800

    Update keywords extract

--- debug/regular_expression.py
+++ debug/regular_expression.py
@@ -11,13 +11,17 @@ import sys
 import importlib
 
 def Extract(aimpath):
-    in_text = aimpath
-    out_text = r"../data/keyword.txt"
-    data_text = r"../data/data.txt"
+    in_text = aimpath#原合同文本
+    out_text = r"../data/keyword.txt"#储存所提取的关键词
+    data_text = r"../data/data.txt"#储存替换后的合同文本
+    map_text = r"../data/map.txt"  # 储存关键词映射
 
     f1 = open(in_text,'r+',encoding='utf8')
     f2 = open(out_text,'w',encoding='utf8')
     f3 = open(data_text, 'w', encoding='utf8')
+    f4 = open(map_text, 'w', encoding='utf8')
+
+    num = 1
 
     for line in f1.readlines():
         try:
@@ -34,10 +38,22 @@ def Extract(aimpath):
             if linkre2:
                 print(linkre2)
                 for keyword in linkre2:
+                    print(keyword)
                     if keyword:
                         f2.write(keyword+'\n')
+
+                        flag = re.findall("_+([^_]*)_+",line)
+                        while flag:
+                            print(flag)
+                            keyword_tran = re.sub('_+([^_]*)_+', '##' + str(num) + '##\n', line, count=1)
+                            f4.write('##' + str(num) + '## ' + flag[0] +'\n')
+                            #print(keyword_tran)
+                            #print(line)
+                            num += 1
+                            line = line.replace(line, keyword_tran)
+                            flag = re.findall("_+([^_]*)_+", line)
             f3.write(line)
-            line = f1.readline()
+            #line = f1.readline()
         except Exception:
             print('error')
             #print(line)
@@ -47,4 +63,5 @@ def Extract(aimpath):
     f1.close()
     f2.close()
     f3.close()
+    f4.close()
 
