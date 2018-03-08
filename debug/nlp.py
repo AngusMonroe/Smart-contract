66de91806194e80b5a31317cdfee3bf56307f0ab
Angus <1621xjx@buaa.edu.cn>
2018-03-08 14:51:00 +0800

    Update doc2txt_only

--- debug/nlp.py
+++ debug/nlp.py
@@ -2,13 +2,13 @@ __author__='XJX, LW'
 __date__='2018.02.23'
 # -*- coding: utf-8 -*-
 
-from doc2txt import *
+from doc2txt_only import *
 from regular_expression import *
 from keywords import *
 import logging
 
 def nlp():
-    mypath = r'../data/datadir'
+    mypath = r'../data/datadir/租赁合同.docx'
     aimpath = Translate(mypath)
     Extract(aimpath)
     Keywords()
